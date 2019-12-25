33f23077b5dc0249b43a7b36aa142473df12f140
Alexander Huber <alih83@gmx.de>
2019-12-25 14:54:27 +0100

    rails and ruby update

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>Home!</h1>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <h1>Home!</h1>
+    </>
+  );
+};
 
 export default App;
